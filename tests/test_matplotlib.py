b443e6e2b0a4baad59b5ae26f9c0bf4e3237a943
Gyeongjae Choi <def6488@gmail.com>
2022-11-01 10:44:57 +0900

    Enable test CI (#23)

---
+++ tests/test_matplotlib.py
@@ -0,0 +1,72 @@
+from conftest import matplotlib_test_decorator
+from pytest_pyodide import run_in_pyodide
+
+
+@matplotlib_test_decorator
+@run_in_pyodide(packages=["matplotlib"])
+def test_plot(selenium_standalone_matplotlib):
+    from matplotlib import pyplot as plt
+
+    plt.figure()
+    plt.plot([1, 2, 3])
+    plt.show()
+
+
+@matplotlib_test_decorator
+@run_in_pyodide(packages=["matplotlib"])
+def test_svg(selenium_standalone_matplotlib):
+    import io
+
+    from matplotlib import pyplot as plt
+
+    plt.figure()
+    plt.plot([1, 2, 3])
+    fd = io.BytesIO()
+    plt.savefig(fd, format="svg")
+
+    content = fd.getvalue().decode("utf8")
+    assert len(content) == 14998
+    assert content.startswith("<?xml")
+
+
+@matplotlib_test_decorator
+@run_in_pyodide(packages=["matplotlib"])
+def test_pdf(selenium_standalone_matplotlib):
+    from matplotlib import pyplot as plt
+
+    plt.figure()
+    plt.plot([1, 2, 3])
+    import io
+
+    fd = io.BytesIO()
+    plt.savefig(fd, format="pdf")
+
+
+@run_in_pyodide(packages=["matplotlib"])
+def test_font_manager(selenium_standalone_matplotlib):
+    """
+    Comparing vendored fontlist.json version with the one built
+    by font_manager.py.
+
+    If you try to update Matplotlib and this test fails, try to
+    update fontlist.json.
+    """
+    import json
+    import os
+
+    from matplotlib import font_manager as fm
+
+    # get fontlist form file
+    fontist_file = os.path.join(os.path.dirname(fm.__file__), "fontlist.json")
+    with open(fontist_file) as f:
+        fontlist_vendor = json.loads(f.read())
+
+    # get fontlist from build
+    fontlist_built = json.loads(json.dumps(fm.FontManager(), cls=fm._JSONEncoder))
+
+    # reodering list to compare
+    for list in ("afmlist", "ttflist"):
+        for fontlist in (fontlist_vendor, fontlist_built):
+            fontlist[list].sort(key=lambda x: x["fname"])
+
+    assert fontlist_built == fontlist_vendor
