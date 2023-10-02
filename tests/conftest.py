e0c0de74c0659cb483f6b81e18f5a864baa71d95
Max Goodridge <youtube@maxgoodridge.com>
2023-10-03 00:46:10 +0100

    Peg PyPA build dependency version

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,11 +20,11 @@ def matplotlib_test_decorator(f):
 def wheel_path(tmp_path_factory):
     # Build a micropip wheel for testing
     import build
-    from build.env import DefaultIsolatedEnv
+    from build.env import IsolatedEnvBuilder
 
     output_dir = tmp_path_factory.mktemp("wheel")
 
-    with DefaultIsolatedEnv() as env:
+    with IsolatedEnvBuilder() as env:
         builder = build.ProjectBuilder(Path(__file__).parent.parent)
         builder.python_executable = env.executable
         builder.scripts_dir = env.scripts_dir
