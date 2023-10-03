cacc993f7da5dc8f615232b2035d79ff6cc9b383
Max Goodridge <youtube@maxgoodridge.com>
2023-10-03 01:01:15 +0100

    Add more args and kwargs to .show() interface

--- matplotlib_pyodide/wasm_backend.py
+++ matplotlib_pyodide/wasm_backend.py
@@ -92,8 +92,8 @@ class FigureManagerAggWasm(FigureManagerBase):
         self.set_window_title("Figure %d" % num)
         self.toolbar = NavigationToolbar2AggWasm(canvas)
 
-    def show(self):
-        self.canvas.show()
+    def show(self, *args, **kwargs):
+        self.canvas.show(*args, **kwargs)
 
     def resize(self, w, h):
         pass
