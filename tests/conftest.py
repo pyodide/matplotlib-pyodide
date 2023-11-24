f9c92cd2e580459c275745e6acbc64edf09d8d6d
ryanking13 <def6488@gmail.com>
2023-11-24 17:26:23 +0900

    Upgrade build version

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,11 +20,11 @@ def matplotlib_test_decorator(f):
 def wheel_path(tmp_path_factory):
     # Build a micropip wheel for testing
     import build
-    from build.env import IsolatedEnvBuilder
+    from build.env import DefaultIsolatedEnv
 
     output_dir = tmp_path_factory.mktemp("wheel")
 
-    with IsolatedEnvBuilder() as env:
+    with DefaultIsolatedEnv() as env:
         builder = build.ProjectBuilder(Path(__file__).parent.parent)
         builder.python_executable = env.executable
         builder.scripts_dir = env.scripts_dir
